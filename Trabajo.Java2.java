0281ef92f2c26399ba9b3f3f687e05332a9d5f09
Vargas201224 <168314453+Vargas201224@users.noreply.github.com>
2024-05-01 14:09:02 -0600

    Add files via upload

---
+++ Trabajo.Java2.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static int tamv = 0;
+    public static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+
+        boolean condiciosalida = true;
+
+        do {
+
+            try {
+
+                System.out.println(
+                        "Ingrese el tamaño del arreglo que no supere los 16 y sea mayor a 1 y que tampoco sea negativo ");
+                tamv = leer.nextInt();//10
+                condiciosalida = (tamv >= 2 && tamv <= 16) ? false : true;
+
+            } catch (Exception e) {
+                System.out.println(e);
+                System.out.println("Ocurrio algo revisar");
+                leer.nextLine();
+            }
+
+        } while (condiciosalida);
+
+        //recibimos el array del metodo
+        int array[] = llenado();
+
+        System.out.println("Mostrar");
+
+        Mostrar(array);
+
+        System.out.println(x:"Mostrar");
+
+        MostrarInverso(array);
+
+    }
+
+    public static int[] llenado() {
+
+        int[] arreglo = new int[tamv];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("ingrese el valo [" + i + "]");
+            arreglo[i] = leer.nextInt();
+        }
+
+        return arreglo;
+    }
+
+    public static void Mostrar(int[] a) {
+
+        for (int i = a.length-1; i>= 0; i-- ) {
+            System.out.println(a [i]);
+        }
+
+        
+
+    }
+
+    
+
+}
